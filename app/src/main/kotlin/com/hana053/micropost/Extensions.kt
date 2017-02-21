0cde1dec733fe1aac510486ca1a93da76946d776
Akira Sosa <akirasosa@gmail.com>
2017-02-21 09:42:11 +0700

    fix getting java class

--- app/src/main/kotlin/com/hana053/micropost/Extensions.kt
+++ app/src/main/kotlin/com/hana053/micropost/Extensions.kt
@@ -3,4 +3,4 @@ package com.hana053.micropost
 import android.app.Activity
 
 fun Activity.getOverridingModule() =
-    (application as BaseApplication).getOverridingModule(this::class.java)
+    (application as BaseApplication).getOverridingModule(javaClass)
